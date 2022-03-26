3389f7a6c8352c1e9bb063450c2d91163b36d844
Rodrigo Lago <rodrigo.n.lago@gmail.com>
2022-03-26 15:59:50 -0300

    upload

--- src/styled/MediaCardContainer.styled.js
+++ src/styled/MediaCardContainer.styled.js
@@ -5,7 +5,6 @@ export const MediaCardContainer = styled.div`
   gap: 30px;
   grid-auto-rows: 215px;
   grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
-}
 
   @media (min-width: 866px){
     grid-template-columns: repeat(auto-fit, minmax(12rem, 2fr));
